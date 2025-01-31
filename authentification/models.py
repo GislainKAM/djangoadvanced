60cfa109f03a7881abab92ed29962edadf13808a
GislainKAM <gislainkamfotso@gmail.com>
2025-01-31 10:03:34 +0100

    creaction du modele utilisateur

--- authentification/models.py
+++ authentification/models.py
@@ -1,3 +1,18 @@
+from msilib.schema import Verb
+from pyexpat import model
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class User(AbstractUser):
+    CREATOR = "CREATOR"
+    SUBSCRIBER = "SUBCRIBER"
+
+    ROLE_CHOICES = (
+        (CREATOR, 'Createur'),
+        (SUBSCRIBER, 'Abonne'),
+        )
+    profile_photo = models.ImageField(verbose_name="photo de profile")
+    role = models.CharField(max_length=30, choices=ROLE_CHOICES, verbose_name="Role")
+
+
